d556d01103197cb5ffbe8e38fa372bd91ca2a523
aman-1004 <2020ceb1004@iitrpr.ac.in>
2023-05-13 02:04:22 +0530

    removed env from gitignore !!!

---
+++ env.py
@@ -0,0 +1,3 @@
+EMAIL_ID='depprojcsece20@gmail.com'
+OTP_URL='https://script.google.com/macros/s/AKfycbx10ufHxvAHbl97_Vjb9-alILtgm-HJ8IYrZX-4g04RfgAKVxOKlLzve5-2DelXZdZw/exec'
+REMINDER_URL='https://script.google.com/macros/s/AKfycbz-6CsoX7wt1WXtADgqpmq6aTzNgcI5vzRnB6lEF9aAQn6DU-dKeODScqdX-X_ybCu2hw/exec'
